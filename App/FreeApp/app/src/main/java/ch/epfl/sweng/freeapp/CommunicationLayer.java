969c83444946e5d6ec26e08cd4fbb91f8164b8f2
Francis <francis.damachi@epfl.ch>
2015-10-22 17:48:12 +0200

    creating way to send Registration response to App

--- App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/CommunicationLayer.java
+++ App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/CommunicationLayer.java
@@ -1,13 +1,30 @@
 package ch.epfl.sweng.freeapp;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by francisdamachi on 22/10/15.
+ * Created by lois on 10/20/15.
  */
 public class CommunicationLayer {
 
+    private static final String SERVER_URL = "http://sweng-wiinotfit.appspot.com";
+    private NetworkProvider networkProvider;
+    private final static int HTTP_SUCCESS_START = 200;
+    private final static int HTTP_SUCCESS_END = 299;
+    private enum ResponseStatus {OK, PASSWORD, USERNAME, EMAIL};
 
-    private static final String SERVER_URL = "HttpExample";
-    private String serverUrl;
 
     /**
      * Creates a new CommunicationLayer instance that communicates with the
@@ -17,10 +34,9 @@ public class CommunicationLayer {
      * Its purposes are:
      * 1.Establish a connection with the server
      * 2.Get responses from the server and return them to the app
-     * @param serverUrl the base URL for the server
      */
-    public CommunicationLayer(String serverUrl) {
-        this.serverUrl = serverUrl;
+    public CommunicationLayer(NetworkProvider networkProvider) {
+        this.networkProvider = networkProvider;
     }
 
     /**
@@ -29,10 +45,37 @@ public class CommunicationLayer {
      * @param logInInfo
      * @return "ok" if the operation was successful, or "failed" otherwise
      */
-    public String sendLogInInfo(LogInInfo logInInfo) {
+    public ResponseStatus sendLogInInfo(LogInInfo logInInfo) throws CommunicationLayerException {
         //Note: needs to use a method from the server (such as myServer.checkLogInInfo(logInInfo))
 
-        return null;
+        try {
+            URL url = new URL(SERVER_URL + "/login?user=[" + logInInfo.getUsername() +"]&password=[" + logInInfo.getPassword() + ")]");
+            HttpURLConnection conn = networkProvider.getConnection(url);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            int response = conn.getResponseCode();
+            if (response < HTTP_SUCCESS_START || response > HTTP_SUCCESS_END) {
+                throw new CommunicationLayerException("Invalid HTTP response code");
+            }
+
+            String serverResponse = fetchContent(conn);
+
+            if(serverResponse.contains("failure")){
+                if(serverResponse.contains("user")){
+                    return ResponseStatus.USERNAME;
+                } else if(serverResponse.contains("password")){
+                    return ResponseStatus.PASSWORD;
+                }
+            }
+
+            return ResponseStatus.OK;
+
+        } catch (IOException e) {
+            throw new CommunicationLayerException();
+        }
+
     }
 
     /**
@@ -41,9 +84,81 @@ public class CommunicationLayer {
      * @param registrationInfo
      * @return "ok" if the operation was successful, or "failed" otherwise
      */
-    public String sendRegistrationInfo(RegistrationInfo registrationInfo){
+    public ResponseStatus sendRegistrationInfo(RegistrationInfo registrationInfo)  throws CommunicationLayerException {
         //Note: needs to use a method from the server (such as myServer.createAccount(registrationInfo))
+
+        if(registrationInfo == null ){
+            throw new CommunicationLayerException("null registrationInfo");
+        }
+
+        try {
+
+            URL url = new URL(SERVER_URL + "register?user="+registrationInfo.getUsername()+"&password="+getPassword()+"&email="+getEmail());
+            HttpURLConnection conn = networkProvider.getConnection(url);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            int response = conn.getResponseCode();
+            if (response < HTTP_SUCCESS_START || response > HTTP_SUCCESS_END) {
+                throw new CommunicationLayerException("Invalid HTTP response code");
+            }
+
+            String serverResponse = fetchContent(conn);
+            JSONObject jsonObject = new JSONObject(serverResponse);
+
+            JSONObject serverResponseJson = jsonObject.getJSONObject("register");
+
+            if(serverResponseJson.getString("status").equals("failure")){
+
+                switch(serverResponseJson.getString("reason")){
+
+                    case "email" : return ResponseStatus.EMAIL;
+                        break;
+                    case "password": return ResponseStatus.PASSWORD;
+                        break;
+                    case "user"  : return ResponseStatus.EMAIL;
+                        break;
+
+                    default: throw new CommunicationLayerException();
+                }
+            }
+
+            assert(serverResponseJson.getString("status").equals("ok"));
+
+            return  ResponseStatus.OK;
+
+        }catch(IOException e ){
+            throw new CommunicationLayerException();
+        }catch(JSONException e ){
+            throw new CommunicationLayerException();
+        }
 
         return null;
     }
+
+
+    private String fetchContent(HttpURLConnection conn) throws IOException {
+        StringBuilder out = new StringBuilder();
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+
+            String result = out.toString();
+            Log.d("HTTPFetchContent", "Fetched string of length "
+                    + result.length());
+            return result;
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
+
 }
---
+++ App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/ServerStatusMessage.java
@@ -0,0 +1,11 @@
+package ch.epfl.sweng.freeapp;
+
+/**
+ * Created by francisdamachi on 22/10/15.
+ */
+public enum ServerStatusMessage {
+
+    OK,
+    USERFAILURE,
+    PASSWORDFAILURE
+}
