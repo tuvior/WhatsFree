a89afc6b9e9cacd6bee576aa23b254301c967363
Pierre Armel Mbanga Ndjock <pierre.mbangandjock@epfl.ch>
2015-10-24 13:30:47 +0200

    before fetching and merging francis' work

--- App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/RegistrationInfo.java
+++ App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/RegistrationInfo.java
@@ -4,4 +4,26 @@ package ch.epfl.sweng.freeapp;
  * Created by francisdamachi on 22/10/15.
  */
 public class RegistrationInfo {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public RegistrationInfo(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
