4964575c4099bc9acd6ff2658853141068f5b598
Francis <francis.damachi@epfl.ch>
2015-11-06 21:38:35 +0100

    dummy Test Created

--- App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/CreateNewActivityTest.java
+++ App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/CreateNewActivityTest.java
@@ -1,9 +1,63 @@
 package ch.epfl.sweng.freeapp;
 
+import org.junit.Test;
+
 /**
  * Created by francisdamachi on 06/11/15.
  */
 public class CreateNewActivityTest {
+
+
+
+    @Test
+    public void testUnableToPickPastDate(){
+
+
+    }
+
+    //User should not be able to create a new submission if a Mandatory field isn't done
+    @Test
+    public void testMandatoryFieldsNotCompleted(){
+
+
+
+    }
+
+    @Test
+    public void testCanCreateNewSubmission(){
+
+
+
+    }
+
+    //If the event happens to happen on the same day, the user should only be able to pick times
+    //equal or greater than the current time
+    @Test
+    public void testCannotPickTimeLessThanCurrentTime(){
+
+
+
+    }
+
+    //if the user sets the time of the event to be at midnight, the date should be increased by a day automatically
+    @Test
+    public void testMidnightEventDateChange(){
+
+
+    }
+
+
+
+    @Test
+    public void testStartingTimeShouldBeAlwaysLargerThanEndTime(){
+
+
+
+
+    }
+
+
+
 
 
 
