5eb0646a5c9b67e41b8aaf5612cfbe115c33b44e
Francis <francis.damachi@epfl.ch>
2015-11-07 15:00:31 +0100

    setting date widget dialog

--- App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/CreateNewSubmissionActivity.java
+++ App/FreeApp/app/src/main/java/ch/epfl/sweng/freeapp/CreateNewSubmissionActivity.java
@@ -1,24 +1,36 @@
 package ch.epfl.sweng.freeapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class CreateNewSubmissionActivity extends AppCompatActivity {
+
+    private Calendar currentCalendar = Calendar.getInstance();
+    private int startYear  = currentCalendar.get(Calendar.YEAR);
+    private int startMonth = currentCalendar.get(Calendar.MONTH);
+    private int startDay   = currentCalendar.get(Calendar.DAY_OF_MONTH);
+    private int DATE_DIALOG_ID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_create_new_submission2);
 
         TextView categoriesText = (TextView)findViewById(R.id.categoriesText);
         categoriesText.setTextSize(19);
+
+
 
         Spinner spinner = (Spinner)findViewById(R.id.categories);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories,android.R.layout.simple_spinner_dropdown_item);
@@ -35,6 +47,19 @@ public class CreateNewSubmissionActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -48,6 +73,26 @@ public class CreateNewSubmissionActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSetDateClicked(View view){
+
+         DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                TextView dateTextView = (TextView)findViewById(R.id.date);
+                dateTextView.setText(dayOfMonth +"-"+monthOfYear + "-"+ year );
+            }
+        };
+
+
+        DatePickerDialog dialogPicker = new DatePickerDialog(this, datePickerListener,this.startYear,this.startMonth,this.startDay);
+        dialogPicker.show();
+
+
+
+
     }
 
 
