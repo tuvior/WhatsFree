2f261da1669ff49937dc55113fe36db8fc6f0788
Loïs <lois.talagrand@epfl.ch>
2015-11-09 18:23:39 +0100

    Removed useless test

--- App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/MainScreenActivityTest.java
+++ App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/MainScreenActivityTest.java
@@ -16,25 +16,19 @@ public class MainScreenActivityTest extends ActivityInstrumentationTestCase2<Mai
         super(MainScreenActivity.class);
     }
 
+    /**
     @Override
     public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
     }
-
-    public void testCanGreetUsers() {
-        getActivity();
-        onView(withId(R.id.mainName)).perform(typeText("from my unit test"));
-        onView(withId(R.id.mainGoButton)).perform(click());
-        //onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
-    }
+    **/
 
     /**
      * Makes sure the What's new tab is displayed by default
      */
     @Test
     public void testDefaultTab(){
-        getActivity();
 
     }
 
@@ -47,9 +41,5 @@ public class MainScreenActivityTest extends ActivityInstrumentationTestCase2<Mai
     public void testClickAroundYou(){
 
     }
-
-    //Other tests:
-    //search button
-    //map button
 
 }
