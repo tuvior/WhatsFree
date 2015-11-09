1f01ba2d1d7ff8c1b03337a0eb60d78629aaedde
Francis <francis.damachi@epfl.ch>
2015-11-09 14:40:20 +0100

    camera correcting

--- App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/CreateNewActivityTest.java
+++ App/FreeApp/app/src/androidTest/java/ch/epfl/sweng/freeapp/CreateNewActivityTest.java
@@ -1,11 +1,9 @@
 package ch.epfl.sweng.freeapp;
 
-import org.junit.Test;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * Created by francisdamachi on 06/11/15.
@@ -13,9 +11,48 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class CreateNewActivityTest {
 
 
+   // @Rule
+    //public ActivityTestRule<CreateNewSubmissionActivity> rule = new ActivityTestRule<>(CreateNewSubmissionActivity.class);
+
+
+    @Rule
+    public IntentsTestRule<CreateNewSubmissionActivity> activityIntentsTestRule = new IntentsTestRule<>(CreateNewSubmissionActivity.class);
 
     @Test
+    public void testSuccessfullyTakesPictureAndSavesIt(){
+
+      //  onView(withId(R.id.takePictureButton)).check(matches(isEnabled()));
+
+
+        /*
+
+        // Create a bitmap we can use for our simulated camera image
+        Bitmap icon = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().getResources(), R.mipmap.ic_launcher);
+
+        ///CreateNewSubmissionActivity createNewSubmissionActivity = rule.getActivity();
+
+        // Build a result to return from the Camera app
+        Intent resultData = new Intent();
+        resultData.putExtra("data", icon);
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        // Stub out the Camera. When an intent is sent to the Camera, this tells Espresso to respond
+        // with the ActivityResult we just created
+        intending(toPackage("com.android.camera2")).respondWith(result);
+
+        onView(withId(R.id.takePictureButton)).perform(click());
+
+        // We can also validate that an intent resolving to the "camera" activity has been sent out by our app
+        intended(toPackage("com.android.camera2"));
+
+*/
+    }
+
+
+/*
+    @Test
     public void testUnableToPickPastDate(){
+
         onView(withId(R.id.setDateButton)).perform(click());
         onView()
 
@@ -26,7 +63,7 @@ public class CreateNewActivityTest {
     public void testMandatoryFieldsNotCompleted(){
 
         onView(withId(R.id.NameOfEvent)).perform(typeText("Free Food"));
-        onView(withId(R.id.Description)).perform(typeText("I am describing the event"));
+     onView(withId(R.id.Description)).perform(typeText("I am describing the event"));
         onView(withId(R.id.Location)).perform(typeText("Ecublens EPFl"));
 
 
@@ -72,6 +109,7 @@ public class CreateNewActivityTest {
 
 
 
+*/
 
 
 
